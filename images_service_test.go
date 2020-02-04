dc37fc2098c2ceb3d2a5f5a4301b50fef899786d
Daniel Redondo <tonyredondo@gmail.com>
2020-02-04 22:53:39 +0100

    tests

--- images_service_test.go
+++ images_service_test.go
@@ -1,21 +1,57 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"go.undefinedlabs.com/scopeagent"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 const restaurantId = "03d207b0-8015-4ab8-950b-8155b87e1654"
 
-func TestGetImagesByRestaurant(t *testing.T) {
-	ctx := scopeagent.GetContextFromTest(t)
+func TestImagesService(t *testing.T) {
+	test := scopeagent.GetTest(t)
 
-	imgs, err := GetImagesByRestaurant(ctx, restaurantId)
-	if err != nil {
-		panic(err)
-	}
+	var images []string
+	test.Run("GetImagesByRestaurant", func(t *testing.T) {
+		ctx := scopeagent.GetContextFromTest(t)
 
-	if imgs == nil {
-		t.Fatal("images is nil")
+		router := setupRouter()
+		url := fmt.Sprintf("/restaurants/%s/images", restaurantId)
+		req, _ := http.NewRequestWithContext(ctx, "GET", url, nil)
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
+		res := w.Result()
+
+		if res.StatusCode != http.StatusOK {
+			t.Fatalf("server: %s respond: %d: %s", url, res.StatusCode, res.Status)
+		}
+		json.NewDecoder(res.Body).Decode(&images)
+		if images == nil {
+			t.Fatal("images can't be nil")
+		}
+	})
+
+	for _, img := range images {
+		t.Run("GetImage:"+img, func(t *testing.T) {
+			ctx := scopeagent.GetContextFromTest(t)
+
+			router := setupRouter()
+			url := fmt.Sprintf("/images/%s", img)
+			req, _ := http.NewRequestWithContext(ctx, "GET", url, nil)
+			w := httptest.NewRecorder()
+			router.ServeHTTP(w, req)
+			res := w.Result()
+
+			if res.StatusCode != http.StatusOK {
+				t.Fatalf("server: %s respond: %d: %s", url, res.StatusCode, res.Status)
+			}
+
+			if res.ContentLength == 0 {
+				t.Fatal("content length is nil")
+			}
+		})
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"go.undefinedlabs.com/scopeagent"
 	"go.undefinedlabs.com/scopeagent/agent"
 	"go.undefinedlabs.com/scopeagent/instrumentation/nethttp"
@@ -13,4 +14,12 @@ func TestMain(m *testing.M) {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Llongfile)
 	nethttp.PatchHttpDefaultClient(nethttp.WithPayloadInstrumentation())
 	os.Exit(scopeagent.Run(m, agent.WithSetGlobalTracer(), agent.WithDebugEnabled()))
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	addImageServiceEndpoints(r)
+	addRatingServiceEndpoints(r)
+	addRestaurantServiceEndpoints(r)
+	return r
 }
