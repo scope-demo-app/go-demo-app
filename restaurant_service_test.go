2a9c39484b90bf4d3c569e7f352611a4cc31a6f8
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2020-03-31 13:35:58 +0200

    Update go-demo-app

--- restaurant_service_test.go
+++ restaurant_service_test.go
@@ -70,31 +70,6 @@ func TestRestaurantService(t *testing.T) {
 		t.Log(rsPayload)
 	})
 
-	test.Run("get-demo", func(t *testing.T) {
-		ctx := scopeagent.GetContextFromTest(t)
-		t.Log("getting restaurant")
-
-		url := fmt.Sprintf("/restaurants/%s", rsPayload.Id)
-		req, _ := http.NewRequestWithContext(ctx, "GET", url, nil)
-		w := httptest.NewRecorder()
-		router.ServeHTTP(w, req)
-		res := w.Result()
-
-		if res.StatusCode != http.StatusOK {
-			t.Fatalf("server: %s respond: %d: %s", url, res.StatusCode, res.Status)
-		}
-
-		var resPayload restaurant
-		err := json.NewDecoder(res.Body).Decode(&resPayload)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if ctx.Err() != nil {
-			t.Fatal(ctx.Err())
-		}
-		t.Log(resPayload)
-	})
-
 	test.Run("delete-demo", func(t *testing.T) {
 		ctx := scopeagent.GetContextFromTest(t)
 		t.Log("deleting restaurant")
